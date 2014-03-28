a2b927fd4be148a13c90c987a67dd7048b870475
Ahmed Alajrami <ahmedalajrami@Ahmeds-MacBook-Pro.local>
2014-03-28 17:26:04 +0000

    Ahmed-2803-updating search tests

--- test/test_search.py
+++ test/test_search.py
@@ -33,7 +33,13 @@ class TestDatabase(unittest.TestCase):
         db.read(path.join(self.data_dir, "dblp_curated_sample.xml"))
         #data = db.search_author_by_name("author1")
         self.assertEqual(db.search_author_by_name("Steffen","1,1","3"),[["Bernhard Steffen",1,0,0,0,1,9]])   
-         
+     
+    def test_serch_author_by_name_sorting_equals(self):
+        db = database.Database()
+        db.read(path.join(self.data_dir, "dblp_curated_sample.xml"))
+        #data = db.search_author_by_name("author1")
+        self.assertEqual(db.search_author_by_name("Carl","2,1","1"),[["Carlo Zaniolo",0,0,1,0,1,5],["Carlos Eduardo Scheidegger",0,1,0,0,1,49],["Carlo Siciliano",1,0,0,0,1,2],["Carlos N. Cumberbatch",1,0,0,0,1,2],["Giancarlo Martella",1,1,0,0,2,2],["Carl Kesselman",2,0,0,0,2,2],["Carlo Conserva", 2, 0, 0, 0, 2, 10],["Carlo Batini", 6, 3, 1, 0, 10, 15]])
+        
         
 if __name__ == '__main__':
     unittest.main()
