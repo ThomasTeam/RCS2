32e9fc919a518755f30aed312ba41efca1a642d8
Ahmed Alajrami <ahmedalajrami@Ahmeds-MacBook-Pro.local>
2014-04-04 17:21:14 +0100

    AhmedShawnEssam-0404-updating author distance tes

--- test/test_author_distance.py
+++ test/test_author_distance.py
@@ -10,15 +10,26 @@ class TestDatabase(unittest.TestCase):
         self.data_dir = path.join(dir, "..", "data")
 
         
-    def test_get_author_distance(self):
+    def test_get_author_distance_04(self):
         db = database.Database()
-        db.read(path.join(self.data_dir, "dblp_curated_sample.xml"))
+        db.read(path.join(self.data_dir, "dblp_sorting_example.xml"))
         #data = db.search_author_by_name("author1")
-        d = db.get_author_distance(34,22)
-        self.assertEqual(d,"3")
+        d = db.get_author_distance(0,4)# Alon Y. Halevy & Philip A. Bernstein
+        self.assertEqual(d,"0")
           
-
-         
+    def test_get_author_distance_45(self):
+        db = database.Database()
+        db.read(path.join(self.data_dir, "dblp_sorting_example.xml"))
+        #data = db.search_author_by_name("author1")
+        d = db.get_author_distance(4,5)# Natalya Fridman Noy & Philip A. Bernstein
+        self.assertEqual(d,"X")
+          
+    def test_get_author_distance_35(self):
+        db = database.Database()
+        db.read(path.join(self.data_dir, "dblp_sorting_example.xml"))
+        #data = db.search_author_by_name("author1")
+        d = db.get_author_distance(3,5)# Natalya Fridman Noy & Pedro Domingos
+        self.assertEqual(d,"1")     
    
         
 if __name__ == '__main__':
