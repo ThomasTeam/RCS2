52627e6b3ef2980d0cb28918976806722616afd1
eelzinaty <engessam1985@gmail.com>
2014-04-05 16:49:35 +0100

    Issam/Shawn/Ahmed-05-04 Adding Style

--- src/comp61542/views.py
+++ src/comp61542/views.py
@@ -12,6 +12,16 @@ def format_data(data):
             result.append((fmt % item).rstrip('0').rstrip('.'))
     return result
 
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+import os
+@app.route('/js/<path:path>')
+def static_proxy(path):
+    # send_static_file will guess the correct MIME type
+    return app.send_static_file(os.path.join('js', path))
+
 @app.route("/averages")
 def showAverages():
     dataset = app.config['DATASET']
@@ -89,11 +99,11 @@ def showCoAuthors():
     args["pub_str"] = PUB_TYPES[pub_type]
     return render_template("coauthors.html", args=args)
 
-@app.route("/")
-def showStatisticsMenu():
-    dataset = app.config['DATASET']
-    args = {"dataset":dataset}
-    return render_template('statistics.html', args=args)
+#@app.route("/")
+#def showStatisticsMenu():
+#    dataset = app.config['DATASET']
+#    args = {"dataset":dataset}
+#    return render_template('statistics.html', args=args)
 
 @app.route("/collaboration/<query>")
 def getCollaborationPage(query):
